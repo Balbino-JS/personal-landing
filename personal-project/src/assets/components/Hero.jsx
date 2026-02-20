e7b6b01d0799ae476d18bbfc959c61e7a945ba7e
Balbino-teste <balbinojteste@gmail.com>
2026-02-20 09:14:24 -0300

    Seção em que criado a Hero e aplicado o css.

---
+++ personal-project/src/assets/components/Hero.jsx
@@ -0,0 +1,20 @@
+import "./components/Hero.css"
+function Hero(){
+
+    return (
+        <section id="home" className="hero">
+            <div className="hero-text">
+                <h1>Transforme seu corpo em 90 dias</h1>
+                <p>Treinnameto personalizados, acompanhamento individual e resultados reais.</p>
+                <a href="#contact" className="hero-btn">Quero mudar agora</a>
+            </div>
+            <div className="hero-images">
+                <img src="https://images.unsplash.com/photo-1599058917212-d750089bc07e" alt="Treino Academia" />
+                <img src="https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b" alt="Personal Trainer" />
+            </div>
+        </section>
+    )
+
+}
+
+export default Hero
--- personal-project/src/main.jsx
+++ personal-project/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import NavBar from './assets/components/NavBar'
+import Hero from './assets/components/Hero'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar />
+    <Hero />
   </StrictMode>,
 )
