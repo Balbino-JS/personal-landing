60a5661acc9443054ce97e03b522e2ba78245bdf
Balbino-teste <balbinojteste@gmail.com>
2026-02-19 21:35:18 -0300

    criando a estrutura no NavBar.

---
+++ personal-project/src/assets/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function NavBar() {
+ 
+
+  return (
+   <nav className='navbar'>
+    <div className='logo'>⚡ FitPro Trainer</div>
+      <ul className='nav-links'>
+        <li><a href="#home">Início</a></li>
+        <li><a href="#services">Serviço</a></li>
+        <li><a href="#testimonials">Resultado</a></li>
+        <li><a href="#contact">Contato</a></li>
+      </ul>
+      <a href="#contact" className='cta-btn'>Começar agora</a>
+    
+   </nav>
+  )
+}
+
+export default NavBar
